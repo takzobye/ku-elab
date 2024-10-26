026681e9d1e7e9015c247d674f89a56c7ea5f0fd
TAKZOBYE <thanathip@zobyeteam.com>
2024-10-26 19:59:57 +0700

    feat: add lab 14

---
+++ lab14/01.py
@@ -0,0 +1,14 @@
+months = {
+    'jan': 'January',
+    'feb': 'February',
+    'mar': 'March',
+    'apr': 'April',
+    'may': 'May',
+    'jun': 'June',
+    'jul': 'July',
+    'aug': 'August',
+    'sep': 'September',
+    'oct': 'October',
+    'nov': 'November',
+    'dec': 'December'
+}
