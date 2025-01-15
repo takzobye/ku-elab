6663b4b1fc09b4d9ed9caa608cb165b604f2a8fc
takzobye <thanathip@zobind.com>
2025-01-16 03:32:03 +0700

    feat(113/lab6/03): finish

---
+++ 113/lab6/03.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define ARRAY_SIZE 98
+
+int main() {
+    int i, j;
+    int A[ARRAY_SIZE] = {0};
+
+    for (i = 2; i < ARRAY_SIZE; i++)
+        A[i] = 1;
+
+    for (i = 2; i < ARRAY_SIZE; i++) {
+        if (A[i] == 1) {
+            for (j = i; j < ARRAY_SIZE; j += i) {
+                if (j != i) {
+                    A[j] = 0;
+                }
+            }
+        }
+    }
+
+    for (i = 2; i < ARRAY_SIZE; i++)
+        if (A[i])
+            printf("%d ", i);
+
+    printf("\n");
+
+    return 0;
+}
